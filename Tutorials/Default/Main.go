65a7f67da5258dde01704119d800f207ae10869c
woitsoh <39669423+woitsoh@users.noreply.github.com>
2021-11-15 22:51:59 +0200

    start tutorial)

--- Tutorials/Default/Main.go
+++ Tutorials/Default/Main.go
@@ -2,6 +2,40 @@ package main
 
 import "fmt"
 
-func main (){
+func main() {
+	//Hello()
+	//Kvadrat()
+	//MultiplyTable()
+	Sum(1, 2, 3)
+	Sum(1, 2, 3, 4, 5)
+}
+
+func Hello() {
 	fmt.Println(`Hello`)
+	fmt.Println(`Hello World`)
+}
+
+func Kvadrat() {
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i, ` ^ 2 = `, i*i)
+
+	}
+}
+
+func MultiplyTable() {
+	for i := 1; i <= 10; i++ {
+		fmt.Print(i, " | ")
+		for j := 1; j <= 10; j++ {
+			fmt.Print(i*j, "\t")
+		}
+		fmt.Println()
+	}
+}
+
+func Sum(numbers ...int) {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	fmt.Println(sum)
 }
