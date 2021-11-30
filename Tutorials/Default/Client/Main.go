b94a015d1ab5be6689a76d477e210acf109b4568
woitsoh <39669423+woitsoh@users.noreply.github.com>
2021-11-30 21:14:44 +0200

    server-client small talk

--- Tutorials/Default/Client/Main.go
+++ Tutorials/Default/Client/Main.go
@@ -5,9 +5,14 @@ import (
 	"io"
 	"net"
 	"os"
+	"time"
 )
 
 func main() {
+	ConnectionWithSever()
+}
+
+func GetMessage() {
 	conn, err := net.Dial("tcp", "127.0.0.1:4545")
 	if err != nil {
 		fmt.Println(err)
@@ -16,4 +21,41 @@ func main() {
 	defer conn.Close()
 	io.Copy(os.Stdout, conn)
 	fmt.Println("\nDone")
+}
+
+func ConnectionWithSever() {
+	conn, err := net.Dial("tcp", "127.0.0.1:4545")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+	for {
+		var source string
+		fmt.Print(`Enter word:`)
+		_, err := fmt.Scanln(&source)
+		if err != nil {
+			fmt.Println(`Need word`, err)
+			continue
+		}
+		n, err := conn.Write([]byte(source))
+		if n == 0 || err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Print(`Association:`)
+		conn.SetReadDeadline(time.Now().Add(time.Second * 3))
+		for {
+			buff := make([]byte, 8)
+			nRead, errRead := conn.Read(buff)
+			if errRead != nil {
+				break
+			}
+			fmt.Print(string(buff[0:nRead]))
+			conn.SetReadDeadline(time.Now().Add(time.Millisecond * 500))
+		}
+
+		fmt.Println()
+
+	}
 }
--- Tutorials/Default/Server/Main.go
+++ Tutorials/Default/Server/Main.go
@@ -5,7 +5,18 @@ import (
 	"net"
 )
 
+var dict = map[string]string{
+	"red":    "hat",
+	"green":  "grass",
+	"blue":   "sky",
+	"yellow": "sun",
+}
+
 func main() {
+	ConnectionWithClient()
+}
+
+func SendMessage() {
 	message := `message from server`
 	listener, err := net.Listen("tcp", ":4545")
 	if err != nil {
@@ -22,5 +33,45 @@ func main() {
 		}
 		conn.Write([]byte(message))
 		conn.Close()
+	}
+}
+
+func ConnectionWithClient() {
+	listener, err := net.Listen("tcp", ":4545")
+	if err != nil {
+		fmt.Println(`Not started, cause `, err)
+		return
+	}
+	defer listener.Close()
+	fmt.Println(`Server is listening`)
+	for {
+		conn, err := listener.Accept()
+		if err != nil { // if error drop connection
+			fmt.Println(`Not accept, cause `, err)
+			conn.Close()
+			continue
+		}
+		go HandleClientRequest(conn)
+	}
+}
+
+func HandleClientRequest(conn net.Conn) {
+	defer conn.Close()
+	for {
+		// get data from request
+		input := make([]byte, 1024*4)
+		n, err := conn.Read(input)
+		if n == 0 || err != nil {
+			fmt.Println("Read error:", err)
+			break
+		}
+		source := string(input[0:n])
+		// get assoсiation
+		target, ok := dict[source]
+		if !ok {
+			target = "undefine word to create association"
+		}
+		fmt.Println(source, "-", target)
+		conn.Write([]byte(target))
 	}
 }
