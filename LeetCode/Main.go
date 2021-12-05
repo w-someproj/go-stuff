b033c6c21e9f93861b497f7608f1bfd1d3583afc
woitsoh <39669423+woitsoh@users.noreply.github.com>
2021-12-05 23:53:28 +0200

    add two numbers (with nodes) - rewrite to faster version

--- LeetCode/Main.go
+++ LeetCode/Main.go
@@ -9,24 +9,11 @@ type ListNode struct {
 
 func main() {
 	//fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	PrintFullNode(addTwoNumbers(GetTwoIntNodes()))
 
-	node1_7 := ListNode{Val: 9}
-	node1_6 := ListNode{Val: 4, Next: &node1_7}
-	node1_5 := ListNode{Val: 2, Next: &node1_6}
-	node1_4 := ListNode{Val: 9, Next: &node1_5}
-	node1_3 := ListNode{Val: 9, Next: &node1_4}
-	node1_2 := ListNode{Val: 9, Next: &node1_3}
-	node1_1 := ListNode{Val: 9, Next: &node1_2}
-
-	node2_4 := ListNode{Val: 9}
-	node2_3 := ListNode{Val: 4, Next: &node2_4}
-	node2_2 := ListNode{Val: 6, Next: &node2_3}
-	node2_1 := ListNode{Val: 5, Next: &node2_2}
-	result := addTwoNumbers(&node1_1, &node2_1)
-	PrintFullNode(result)
 }
 
-// optimized
+// optimized (ez - topics: array, hash table)
 func twoSum(nums []int, target int) []int {
 	sumMap := make(map[int]int)
 	for i, elem := range nums {
@@ -39,34 +26,54 @@ func twoSum(nums []int, target int) []int {
 	return []int{}
 }
 
-// rewrite???
+// this  faster (medium - topics: recursion?, math, linked list)
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	head := l1
 	carry := 0
-	for l1 != nil || l2 != nil || carry != 0 {
-		if l2 != nil {
-			l1.Val += l2.Val
-		}
-		l1.Val += carry
+	for {
+		l1.Val += l2.Val + carry
+
 		carry = l1.Val / 10
 		l1.Val = l1.Val % 10
-		if l1.Next == nil {
-			if l2.Next != nil {
-				l1.Next = l2.Next
-				l2.Next = &ListNode{0, nil}
-			} else if carry != 0 {
-				l1.Next = &ListNode{0, nil}
-			} else {
-				break
-			}
+		if l2.Next == nil {
+			break
+		} else if l1.Next == nil {
+			l1.Next = l2.Next
+			break
 		}
 		l1 = l1.Next
-		if l2.Next == nil {
-			l2.Next = &ListNode{0, nil}
-		}
 		l2 = l2.Next
 	}
+	for carry != 0 {
+		if l1.Next == nil {
+			l1.Next = &ListNode{0, nil}
+		}
+		l1.Next.Val += carry
+
+		carry = l1.Next.Val / 10
+		l1.Next.Val = l1.Next.Val % 10
+
+		l1 = l1.Next
+	}
 	return head
+}
+
+// utility fuctions
+
+func GetTwoIntNodes() (*ListNode, *ListNode) {
+	node1_7 := ListNode{Val: 9}
+	node1_6 := ListNode{Val: 4, Next: &node1_7}
+	node1_5 := ListNode{Val: 2, Next: &node1_6}
+	node1_4 := ListNode{Val: 9, Next: &node1_5}
+	node1_3 := ListNode{Val: 9, Next: &node1_4}
+	node1_2 := ListNode{Val: 9, Next: &node1_3}
+	node1_1 := ListNode{Val: 9, Next: &node1_2}
+
+	node2_4 := ListNode{Val: 9}
+	node2_3 := ListNode{Val: 4, Next: &node2_4}
+	node2_2 := ListNode{Val: 6, Next: &node2_3}
+	node2_1 := ListNode{Val: 5, Next: &node2_2}
+	return &node1_1, &node2_1
 }
 
 func PrintFullNode(l *ListNode) {
