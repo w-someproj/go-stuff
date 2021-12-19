c87e95784a6f6c4c5bf22f46dc41bd4987d31641
woitsoh <39669423+woitsoh@users.noreply.github.com>
2021-12-19 20:34:13 +0200

    Integer to Roman - maybe need some rework but it`s fast

--- LeetCode/Main.go
+++ LeetCode/Main.go
@@ -21,7 +21,8 @@ func main() {
 	//fmt.Println(convert(`AB`, 1))
 	//fmt.Println(reverse(-123))
 	//fmt.Println(isPalindrome(121))
-	fmt.Println(maxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7}))
+	//fmt.Println(maxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7}))
+	fmt.Println(intToRoman(1994))
 
 }
 
@@ -268,6 +269,57 @@ func maxArea(height []int) int {
 		}
 	}
 	return result
+}
+
+//Integer to Roman (medium - topics: hash table, math, string)
+// it`s fast, but maybe need rework with maps? (or 2 arrays better in GO for this task)
+// []string change to []byte and win in memory
+func intToRoman(num int) string {
+	result := []string{}
+	for num > 0 {
+		if num >= 1000 {
+			result = append(result, `M`)
+			num -= 1000
+		} else if num >= 900 {
+			result = append(result, `CM`)
+			num -= 900
+		} else if num >= 500 {
+			result = append(result, `D`)
+			num -= 500
+		} else if num >= 400 {
+			result = append(result, `CD`)
+			num -= 400
+		} else if num >= 100 {
+			result = append(result, `C`)
+			num -= 100
+		} else if num >= 90 {
+			result = append(result, `XC`)
+			num -= 90
+		} else if num >= 50 {
+			result = append(result, `L`)
+			num -= 50
+		} else if num >= 40 {
+			result = append(result, `XL`)
+			num -= 40
+		} else if num >= 10 {
+			result = append(result, `X`)
+			num -= 10
+		} else if num >= 9 {
+			result = append(result, `IX`)
+			num -= 9
+		} else if num >= 5 {
+			result = append(result, `V`)
+			num -= 5
+		} else if num >= 4 {
+			result = append(result, `IV`)
+			num -= 4
+		} else if num >= 1 {
+			result = append(result, `I`)
+			num -= 1
+		}
+	}
+
+	return strings.Join(result, ``)
 }
 
 // utility fuctions
