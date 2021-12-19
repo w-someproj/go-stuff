d8dbc1de61b4a3c7542bb8ff9474b0970868db30
woitsoh <39669423+woitsoh@users.noreply.github.com>
2021-12-19 13:07:10 +0200

    Palindrome Number - fast and not so fast

--- LeetCode/Main.go
+++ LeetCode/Main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -18,7 +19,8 @@ func main() {
 	//fmt.Println(findMedianSortedArrays([]int{1, 2}, []int{3, 4}))
 	//fmt.Println(longestPalindrome(`cabcbad`))
 	//fmt.Println(convert(`AB`, 1))
-	fmt.Println(reverse(-123))
+	//fmt.Println(reverse(-123))
+	fmt.Println(isPalindrome(121))
 
 }
 
@@ -201,6 +203,40 @@ func reverse(x int) int {
 		x = x / 10
 	}
 	return result
+}
+
+//Palindrome Number (easy - topics: math)
+// reverse then check - faster
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	x1 := x
+	result := 0
+	for x != 0 {
+		tail := x % 10
+		result = result*10 + tail
+		x = x / 10
+	}
+	if x1 == result {
+		return true
+	}
+	return false
+}
+
+//Palindrome Number (easy - topics: math)
+// range x and check
+func isPalindromeV(x int) bool {
+	if x < 0 {
+		return false
+	}
+	str := strconv.Itoa(x)
+	for i := 0; i < len(str)/2; i++ {
+		if str[i] != str[len(str)-i-1] {
+			return false
+		}
+	}
+	return true
 }
 
 // utility fuctions
