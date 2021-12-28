148efa8b045086a190c2b2e66ca9ea14bdaa9487
woitsoh <39669423+woitsoh@users.noreply.github.com>
2021-12-29 00:55:32 +0200

    Generate Parentheses - with recursion

--- LeetCode/Main.go
+++ LeetCode/Main.go
@@ -29,7 +29,8 @@ func main() {
 	//fmt.Println(threeSumClosest([]int{1, 1, -1, -1, 3}, 3))
 	//fmt.Println(letterCombinations("23"))
 	//fmt.Println(fourSum([]int{2, 2, 2, 2, 2}, 8))
-	fmt.Println(isValid(`({[][]})`))
+	//fmt.Println(isValid(`({[][]})`))
+	fmt.Println(generateParenthesis(2))
 }
 
 // optimized (ez - topics: array, hash table)
@@ -532,6 +533,28 @@ func isValid(s string) bool {
 		}
 	}
 	return len(stack) == 0
+}
+
+//Generate Parentheses (medium - topics: string, dynamic programming, backtracking)
+func generateParenthesis(n int) []string {
+	var res []string
+	generateParenthesisRecursion(&res, "", 0, 0, n)
+	return res
+}
+
+func generateParenthesisRecursion(result *[]string, parentheses string, left, right, max int) {
+	if len(parentheses) == max*2 {
+		*result = append(*result, parentheses)
+		return
+	}
+
+	if left < max {
+		generateParenthesisRecursion(result, parentheses+"(", left+1, right, max)
+	}
+	if right < left {
+		generateParenthesisRecursion(result, parentheses+")", left, right+1, max)
+	}
+
 }
 
 // utility fuctions
